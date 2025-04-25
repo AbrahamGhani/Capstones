db020b93a7352155190871a6c75dc955918feac3
Abraham Ghani <AbrahamKosmoGhani@gmail.com>
2025-04-25 11:38:12 -0700

    added readme. also started planning for the first capstone. created some classes i might need and made a rough plan

---
+++ Capstone1-AccountingLedger/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+
+       /*
+       0 - tools -- Scanner, filewrite(buffwrite), fileread(buffread),arraylist
+       1 - start by creating the loop for the main menu
+       2 - in the main menu loop create additional loops for the submenus like the ledger menu
+       3 - use scanner to grab user inputs for the menu
+       4 - log transactions into the csv file. for this make a method in the TransactionHelper class to grab user input
+                and format it and create a transaction object to add to a list. write items from list into the file
+       5 - log account balance and time stamps of transactions
+       6 -        
+        */
+
+
+
+
+    }
+}
