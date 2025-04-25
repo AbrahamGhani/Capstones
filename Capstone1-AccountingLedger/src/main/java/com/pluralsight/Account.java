e8dd36133244100cb6b892465e5a2f6652a6135a
Abraham Ghani <AbrahamKosmoGhani@gmail.com>
2025-04-25 12:05:52 -0700

    constructors | getters & setters | added display info method

--- Capstone1-AccountingLedger/src/main/java/com/pluralsight/Account.java
+++ Capstone1-AccountingLedger/src/main/java/com/pluralsight/Account.java
@@ -10,4 +10,45 @@ public class Account {
     String accountID;
 
 
+    public Account(double balance, String owner, String bank, String accountID) {
+        this.balance = balance;
+        this.owner = owner;
+        this.bank = bank;
+        this.accountID = accountID;
+    }
+
+    public Account() {
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getBank() {
+        return bank;
+    }
+
+    public void setBank(String bank) {
+        this.bank = bank;
+    }
+
+    public String getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(String accountID) {
+        this.accountID = accountID;
+    }
 }
--- Capstone1-AccountingLedger/src/main/java/com/pluralsight/Transaction.java
+++ Capstone1-AccountingLedger/src/main/java/com/pluralsight/Transaction.java
@@ -11,5 +11,70 @@ public class Transaction {
     double amount;
 
 
+    public Transaction(String date, String time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public Transaction() {
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+
+    public void CSVFormatDisplayTransactionInfo(){
+        System.out.println(this.getDate() + "|" + this.getTime() + "|" + this.getDescription() + "|"
+                + this.getVendor() + "|" + this.getAmount());
+
+    }
+
+    public void displayTransactionInfo(){
+        System.out.println("Date: " + this.getDate());
+        System.out.println("Time: " + this.getTime());
+        System.out.println("Description: " + this.getDescription());
+        System.out.println("Vendor: " + this.getVendor());
+        System.out.println("Amount: $" + this.getAmount());
+    }
 
 }
